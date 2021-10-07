c427bd4c415d7288e62be6b6c27cfcc19c3f47de
kukazucker <85445965+kukazucker@users.noreply.github.com>
2021-10-07 21:50:14 +0200

    Update sparse_arrays.cpp

--- sparse_arrays.cpp
+++ sparse_arrays.cpp
@@ -21,29 +21,29 @@
  ==================================
  */
 
-std::vector<int> matchingStrings(std::vector<std::string> strings, std::vector<std::string> queries) {
+std::vector<int> matchingStrings( std::vector< std::string > strings, std::vector< std::string > queries ) {
     
-    std::vector<int> sums;                                      // sum of every query
-    sums.resize(queries.size());                                // resize of vector
+    std::vector< int > sums;                                      // sum of every query
+    sums.resize( queries.size() );                                // resize of vector
 
-    for( unsigned int i = 0; i < queries.size(); i++ ) {        // sort of queryes from queries[0]
+    for( unsigned int i = 0; i < queries.size(); i++ ) {          // sort of queryes from queries[0]
         
-        int summa = 0;                                          // sum of every query, updated every cycle
+        int summa = 0;                                            // sum of every query, updated every cycle
 
-        for( unsigned int j = 0; j < strings.size(); j++ ) {    // sort of strings
+        for( unsigned int j = 0; j < strings.size(); j++ ) {      // sort of strings
 
-            if( queries[i] == strings[j] ) {                    // compare the queryes and the strings
+            if( queries[ i ] == strings[ j ] ) {                  // compare the queryes and the strings
                 
-                summa += 1;                                     // plus one concidence every time, updated every cycle
+                summa += 1;                                       // plus one concidence every time, updated every cycle
                 
             }
             
-            sums.at(i) = summa;                                 //return amount to std::vector<int> sums
+            sums.at( i ) = summa;                                 //return amount to std::vector<int> sums
             
         }
     }
     
-    return sums;                                                // return of all amounts
+    return sums;                                                  // return of all amounts
 }
 
 
@@ -55,9 +55,9 @@ int main() {
     std::vector<std::string> querties { "cognac", "cat", "dog", "vodka", "beer" };          // querties
     
     
-    std::vector<int> sums = matchingStrings(strings, querties);                             // function
+    std::vector<int> sums = matchingStrings( strings, querties );                           // function
     
-    for( int i : sums) {
+    for( int i : sums ) {
         
         std::cout << i << "\n";                                                             // display every sum from matchingStrings()
         
