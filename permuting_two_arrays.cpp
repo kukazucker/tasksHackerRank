97b52a315761ad4a16330cf795124780a0eedc71
kukazucker <85445965+kukazucker@users.noreply.github.com>
2021-10-09 11:49:25 +0200

    Update permuting_two_arrays.cpp

--- permuting_two_arrays.cpp
+++ permuting_two_arrays.cpp
@@ -2,9 +2,11 @@
 /*
  =====================================================================================================================================
  
- There are two n-element arrays of integers, A and B. Permute them into some A` and B` such that the relation A`[i] + B`[i] >= k holds for all i where 0 <= i <= n.
+ There are two n-element arrays of integers, A and B. Permute them into some A` and B` such that 
+ the relation A`[i] + B`[i] >= k holds for all i where 0 <= i <= n.
  
- There will be q queries consistion of A, B, and k. For each query, return YES if some permutation A`, B` satisfying the relation exists. Otherwise, return NO;
+ There will be q queries consistion of A, B, and k. For each query, 
+ return YES if some permutation A`, B` satisfying the relation exists. Otherwise, return NO;
  
  =====================================================================================================================================
  */
@@ -14,34 +16,34 @@
 #include <vector>
 
 
-std::string twoArrays(int k, std::vector<int> A, std::vector<int> B) {   // input data
+std::string twoArrays( int k, std::vector< int > A, std::vector< int > B ) {   // input data
     
-    std::sort(A.begin(), A.end());                                       // sort of array from small to big
-    std::sort(B.begin(), B.end(), std::greater<int>());                  // sort of array from big to small
+    std::sort( A.begin(), A.end() );                                           // sort of array from small to big
+    std::sort( B.begin(), B.end(), std::greater<int>() );                      // sort of array from big to small
     
     
-    for( unsigned int i = 0; i < A.size(); i++ ) {                       // compare and add integers A[i] and B[i]
+    for( unsigned int i = 0; i < A.size(); i++ ) {                             // compare and add integers A[i] and B[i]
             
-            if( A[i] + B[i] < k ) {                                      // if less than k then return NO
+            if( A[ i ] + B[ i ] < k ) {                                        // if less than k then return NO
                 
                 return "NO";
                 
             }
     }
     
-    return "YES";                                                        // in another case YES
+    return "YES";                                                              // in another case YES
 }
 
 int main() {
     
 
-    std::vector< int > arrayA {2, 1, 3};                                 // first array
-    std::vector< int > arrayB {7, 8, 9};                                 // second array
+    std::vector< int > arrayA { 2, 1, 3 };                                     // first array
+    std::vector< int > arrayB { 7, 8, 9 };                                     // second array
     
-    int k = 7;                                                           // integer k
+    int k = 7;                                                                 // integer k
 
-    std::string answer = twoArrays(k, arrayA, arrayB);                   // function
-    
+    std::string answer = twoArrays( k, arrayA, arrayB );                       // function
+     
     std::cout << answer;
     
     return 0;
